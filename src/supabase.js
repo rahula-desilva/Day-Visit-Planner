abf7bca421a566ba16212f3da067f7179822e342
Rahula De Silva <rahula.desilva.lk@gmail.com>
2026-04-08 19:10:22 +0530

    Move Supabase config to Vite env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oqxosrxeuyybjjffwnoz.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeG9zcnhldXl5YmpqZmZ3bm96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NDU1NTUsImV4cCI6MjA5MDAyMTU1NX0.OazA8GqbD0j9BVpbTBc8-Mn51mlQz-xqkeTvr8Vjwss";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
