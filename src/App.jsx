19cee8d985b20712281f4d798c0a2d639b6da1eb
Rahula De Silva <rahula.desilva.lk@gmail.com>
2026-04-08 18:01:01 +0530

    Connected Supabase and display trips

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,42 @@ import { useEffect, useState } from "react";
 function App() {
   const [status, setStatus] = useState("Checking...");
 
-  useEffect(() => {
-    checkConnection();
-  }, []);
-
+useEffect(() => {
+  console.log("App Loaded"); // 👈 ADD THIS
+  checkConnection();
+  saveTrip();
+}, []);
   async function checkConnection() {
-    const { data, error } = await supabase.from("trips").select("*");
+    const { data, error } = await supabase.from("places").select("*");
 
     if (error) {
+      console.log("Error:", error); //  ADD THIS
       setStatus("❌ Not Connected");
     } else {
+      console.log("Data:", data); // ADD THIS
       setStatus("✅ Connected Successfully");
     }
   }
 
   return <h1>{status}</h1>;
 }
+async function saveTrip() {
+  const { data, error } = await supabase
+    .from('trips')
+    .insert([
+      {
+        user_id: null,   // temporary (we'll fix auth later)
+        date: "2026-04-08"
+      }
+    ])
+    .select(); // 👈 ADD THIS
+
+  if (error) {
+    console.log("Insert Error:", error)
+  } else {
+    console.log("Trip Saved:", data)
+  }
+}
+
 
 export default App;
